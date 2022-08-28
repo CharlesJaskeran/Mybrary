6bac0d330dafcbb6d5180c84854fcd071bbce766
CharlesJaskeran <charliejaskeran@gmail.com>
2022-08-28 10:38:30 +0100

    Finished Author Routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
@@ -19,6 +20,7 @@ app.set('views', __dirname + '/views')
 //app.set('views', path.join(__dirname, 'views'))
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
